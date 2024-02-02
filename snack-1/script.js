95055c2fe7fcd376e40af42eea40bf75b498d1d2
mario.arista <mario.arista.dev@gmail.com>
2024-02-02 15:32:00 +0100

    starting first snack

---
+++ snack-1/script.js
@@ -0,0 +1,25 @@
+/*
+L’utente inserisce due parole in successione.
+Il software stampa prima la parola più corta, poi la parola più lunga.
+*/
+
+// Inizializzo gli input
+const firstwordElement = document.getElementById("first-word");
+const secondwordElement = document.getElementById("second-word");
+
+
+// Inizializzo output
+const lesswordsElement = document.getElementById("word-less-letters");
+const morewordsElement = document.getElementById("word-more-letters");
+
+// Inizializzo button
+const buttonUserElement = document.getElementById("user-button");
+
+
+
+buttonUserElement.addEventListener("click", function() {
+    
+    
+    lesswordsElement.innerText = 
+
+});
