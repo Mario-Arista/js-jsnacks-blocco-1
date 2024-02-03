dffea9fd17571ec84576aae9361bf0d7a41d8329
mario.arista <mario.arista.dev@gmail.com>
2024-02-03 19:27:04 +0100

    4 snack done

--- snack-4/script.js
+++ snack-4/script.js
@@ -12,8 +12,9 @@ buttonInvio.addEventListener("click",
     event.preventDefault(); 
 
     // Mi salvo il valore dell'input
-    const numberValue = parseInt(numberElement.valueOf);
-    
+
+    const numberValue = Math.floor(numberElement.value);
+
     // Controllo che il numero sia di 4 cifre
     if (numberValue < 1000 || numberValue > 9999 ) {
         alert("Devi inserire un numero di 4 cifre");
@@ -21,15 +22,20 @@ buttonInvio.addEventListener("click",
 
     // Estraggo le cifre
     const migliaia = Math.floor(numberValue / 1000);
-    const centinia = Math.floor(numberValue / 100);
-    const decima = Math.floor(numberValue / 10);
+    console.log(migliaia);
+    const centinaia = Math.floor((numberValue % 1000) / 100);
+    console.log(centinaia);
+    const decima = Math.floor((numberValue % 100) / 10);
+    console.log(decima);
     const unitaFinale = numberValue % 10;
+    console.log(unitaFinale);
+    
 
     // Sommo le cifre
-    const somma = migliaia + centinia + decima + unitaFinale;
+    const somma = migliaia + centinaia + decima + unitaFinale;
 
     // Metto in pagina la somma
-    const outputSumElement = document.getElementById("sum-numbers");
-    outputSumElement.innerText = somma;
-    
+    const outputSumElement = document.querySelector("#results");
+    outputSumElement.innerHTML += somma;
+    document.querySelector("h2").classList.remove("d-none")
 });
